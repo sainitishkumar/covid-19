afcf2586722c7061a0d749151b94d48c042d762b
sainitishkumar <bsainitishkumar@gmail.com>
2020-03-30 16:55:50 +0530

    add/delete locations

--- osm.js
+++ osm.js
@@ -3,7 +3,10 @@ var OSMPICKER = (function(){
 	
 	var map;
 	var marker;
-	var circle;
+    var circle;
+    var marker_array = [];
+    var text_array = [];
+
 	app.initmappicker = function(lat, lon, r, option){
 		try{
 			map = new L.Map('locationPicker');
@@ -30,44 +33,47 @@ var OSMPICKER = (function(){
 			map.setView(e.target.getLatLng());
 			$("#"+option.latitudeId).val(e.target.getLatLng().lat);
 			$("#"+option.longitudeId).val(e.target.getLatLng().lng);
-		});
-		map.addLayer(marker);
-		map.addLayer(circle);
+        });
+		// map.addLayer(marker);
+		// map.addLayer(circle);
 
-		$("#"+option.latitudeId).val(lat);
-		$("#"+option.latitudeId).on('change', function(){
-			marker.setLatLng([Number($(this).val()), marker.getLatLng().lng]);
-			circle.setLatLng(marker.getLatLng());
-			map.setView(marker.getLatLng());
-		});
+		// $("#"+option.latitudeId).val(lat);
+		// $("#"+option.latitudeId).on('change', function(){
+		// 	marker.setLatLng([Number($(this).val()), marker.getLatLng().lng]);
+		// 	circle.setLatLng(marker.getLatLng());
+		// 	map.setView(marker.getLatLng());
+		// });
 
-		$("#"+option.longitudeId).val(lon);
-		$("#"+option.longitudeId).on('change', function(){
-			marker.setLatLng([marker.getLatLng().lat, Number($(this).val())]);
-			circle.setLatLng(marker.getLatLng());
-			map.setView(marker.getLatLng());
-		});
+		// $("#"+option.longitudeId).val(lon);
+		// $("#"+option.longitudeId).on('change', function(){
+		// 	marker.setLatLng([marker.getLatLng().lat, Number($(this).val())]);
+		// 	circle.setLatLng(marker.getLatLng());
+		// 	map.setView(marker.getLatLng());
+		// });
 
 		$("#"+option.radiusId).val(r);
 		$("#"+option.radiusId).on('change', function(){
 			circle.setRadius(Number($(this).val()));
 		});
 
-		$("#"+option.addressId).on('change', function(){
-			var item = searchLocation($(this).val(), newLocation);
-		});
+		// $("#"+option.addressId).on('change', function(){
+		// 	var item = searchLocation($(this).val(), newLocation);
+		// });
+
+        $('#add_button').on('click', function(){
+            var item = searchLocation($("#"+option.addressId).val(), newLocation);
+        });
 
 		function newLocation(item, text){
             if(item!=undefined){
-                $("#"+option.latitudeId).val(item.lat);
-                $("#"+option.longitudeId).val(item.lon);
                 markerloc = new L.LatLng(item.lat, item.lon);
                 marker = new L.marker(markerloc, {draggable:'true'});
-                marker.setLatLng([item.lat, item.lon]);
+                marker_array.push(marker);
+                text_array.push(text);
+                circle.setLatLng(marker.getLatLng());
                 marker.bindPopup(text);
-                circle.setLatLng([item.lat, item.lon]);
-                map.setView([item.lat, item.lon]);
                 map.addLayer(marker);
+                map.setView(marker.getLatLng());
                 map.addLayer(circle);
                 table_text = "<tr>";
                 table_text += "<td>"+text+"</td>";
@@ -79,7 +85,46 @@ var OSMPICKER = (function(){
     
     $("#table_id").on('click', 'button', onClickDelete);
     function onClickDelete() {
-    $(this).parents('tr').remove();
+        console.log($(this).parents('tr')[0].cells[0].innerText);
+        var temp = $(this).parents('tr')[0].cells[0].innerText;
+        $(this).parents('tr').remove();
+        // var num = text_array.findIndex(function(e){
+        //     return e == temp;
+        // });
+        var num;
+        for(var i = 0; i<text_array.length; i++){
+            if(text_array[i] == temp) {
+                num = i;
+                break;
+            }
+        }
+        var temptext = text_array.splice(num,1);
+        var marker = marker_array.splice(num,1);
+        console.log("Removed"+temptext, num);
+        // map.removeLayer(marker);
+        map.eachLayer(function(marker){
+            map.removeLayer(marker);
+        });
+        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+		var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+		var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 40});		
+		map.addLayer(osm);
+        for (var i=0; i<marker_array.length; i++) {
+            // var lon = marker_array[i][0];
+            // var lat = marker_array[i][1];
+            var popupText = text_array[i];
+            // var markerLocation = new L.LatLng(lat, lon);
+            // var marker = new L.Marker(markerLocation);
+            circle.setLatLng(marker_array[i].getLatLng());
+            marker_array[i].bindPopup(popupText);
+            map.addLayer(marker_array[i]);
+            map.setView(marker_array[i].getLatLng());
+            map.addLayer(circle);
+            // table_text = "<tr>";
+            // table_text += "<td>"+popupText+"</td>";
+            // table_text += "<td>" + '<button>Delete</button>' + "</td></tr>";
+            // $("#table_id").append(table_text);
+        }
     }
 
 	function searchLocation(text, callback){
