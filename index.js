bbedfc1e64a15e7c5f1ef711504f24124ba48c8d
sainitishkumar <bsainitishkumar@gmail.com>
2020-03-31 20:33:29 +0530

    test deploy

--- index.js
+++ index.js
@@ -23,6 +23,30 @@ app.get('/osm.js', function(req, res) {
     res.sendFile(path.join(__dirname+'/osm.js'));
 });
 
+app.get('/js/jquery-1.11.3.min.js', function(req, res) {
+    res.sendFile(path.join(__dirname+'/js/jquery-1.11.3.min.js'));
+});
+
+app.get('/js/jquery-migrate-1.2.1.min.js', function(req, res) {
+    res.sendFile(path.join(__dirname+'/osm.html'));
+});
+
+app.get('/js/bootstrap.min.js', function(req, res) {
+    res.sendFile(path.join(__dirname+'/js/bootstrap.min.js'));
+});
+
+app.get('/js/leaflet.js', function(req, res) {
+    res.sendFile(path.join(__dirname+'/js/leaflet.js'));
+});
+
+app.get('/js/images/marker-icon.png', function(req, res) {
+    res.sendFile(path.join(__dirname+'/images//marker-icon.png'));
+});
+
+app.get('/js/images/marker-shadow.png', function(req, res) {
+    res.sendFile(path.join(__dirname+'/images//marker-shadow.png'));
+});
+
 let port = process.env.PORT || 3000;
 
 console.log("port", port);
